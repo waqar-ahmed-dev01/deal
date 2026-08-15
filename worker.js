3d9a6343ec7eeabd1d8ae081dd324922723fe7b5
waqar-ahmed-dev01 <164100154+waqar-ahmed-dev01@users.noreply.github.com>
2026-08-15 12:14:35 +0000

    s

--- worker.js
+++ worker.js
@@ -1,32 +1,21 @@
 const CACHE_NAME = 'dealhub-v1';
-const ASSETS = [
-    '/',
-    '/manifest.json',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
-];
 
-// Install
+// Cache all assets
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(ASSETS))
+            .then(cache => {
+                return cache.addAll([
+                    '/',
+                    '/manifest.json',
+                    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+                ]);
+            })
             .then(() => self.skipWaiting())
     );
 });
 
-// Activate
-self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            );
-        }).then(() => self.clients.claim())
-    );
-});
-
-// Fetch
+// Serve cached content when offline
 self.addEventListener('fetch', (e) => {
     e.respondWith(
         fetch(e.request)
