1ff7f9131f1016d32d9f29539e295de38d3672aa
waqar-ahmed-dev01 <164100154+waqar-ahmed-dev01@users.noreply.github.com>
2026-08-16 09:43:02 +0000

    ds

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dealhub-v1';
+const CACHE_NAME = 'dealhub-v2';
 const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
